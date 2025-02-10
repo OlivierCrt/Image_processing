8cdfe9a7e61eca19be805fe2e4b450869bf02750
OlivierCr <ocrampette@gmail.com>
2025-02-10 17:52:16 +0100

    tp1 op

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -82,3 +82,90 @@ total_processing_time = end_time - start_time
 print(f"Temps total de traitement pour toutes les images: {total_processing_time:.4f} secondes")
 
 print("Traitement terminé !")
+
+
+
+
+
+###################################################################
+                    #Exercice2#
+###################################################################
+
+
+
+
+
+def floyd_steinberg_dithering(image, LUT):
+    """
+    Applique le dithering de Floyd-Steinberg sur une image en couleur.
+
+    Parameters:
+        image (numpy.ndarray): L'image d'entrée en RGB.
+        LUT (numpy.ndarray): La table de correspondance des couleurs.
+
+    Returns:
+        numpy.ndarray: L'image avec dithering appliqué.
+    """
+    h, w, _ = image.shape
+    image = image.astype(np.float64)
+
+    for y in range(h):
+        for x in range(w):
+            old_pixel = image[y, x].copy()
+            closest_color = LUT[np.argmin(np.linalg.norm(LUT - old_pixel, axis=1))]
+            image[y, x] = closest_color
+
+            quant_error = old_pixel - closest_color
+
+            if x < w - 1:
+                image[y, x + 1] += quant_error * 7 / 16
+            if y < h - 1:
+                image[y + 1, x] += quant_error * 5 / 16
+            if x > 0 and y < h - 1:
+                image[y + 1, x - 1] += quant_error * 3 / 16
+            if x < w - 1 and y < h - 1:
+                image[y + 1, x + 1] += quant_error * 1 / 16
+
+    return np.clip(image, 0, 255).astype(np.uint8)
+
+
+input_folder = "/home/python/Image_processing/TP1/Images"
+output_folder = "/home/python/Image_processing/TP1/Dithering"
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+N = 8  
+LUT = LutSubSamp(N)
+print(LUT)
+
+start_time = time.time()
+
+for filename in os.listdir(input_folder):
+    if filename.endswith((".jpg", ".png", ".bmp")):
+        image_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, f"quantized_{filename}")
+        
+        image_start_time = time.time()
+
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        quantized_image = floyd_steinberg_dithering(image, LUT)
+        cv2.imwrite(output_path, cv2.cvtColor(quantized_image, cv2.COLOR_RGB2BGR))
+        print(f"Image quantifiée avec dithering sauvegardée : {output_path}")
+        
+        mse_value = MSE(quantized_image, image)
+        print("Erreur quadratique :", mse_value)
+        
+        image_end_time = time.time()
+        image_processing_time = image_end_time - image_start_time
+
+        print(f"Temps de traitement pour {filename}: {image_processing_time:.4f} secondes")
+        print("\n")
+
+end_time = time.time()
+total_processing_time = end_time - start_time
+print(f"Temps total de traitement pour toutes les images: {total_processing_time:.4f} secondes")
+
+print("Traitement terminé !")
---
+++ TP2/tp2.py
@@ -0,0 +1,127 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+bateaunoir = cv2.imread("/home/python/Image_processing/TP2/Input_images/bateauNoir.bmp", cv2.IMREAD_GRAYSCALE)
+pingu = cv2.imread("/home/python/Image_processing/TP2/Input_images/papaPingu.jpg", cv2.IMREAD_GRAYSCALE)
+seanight = cv2.imread("/home/python/Image_processing/TP2/Input_images/seanight.bmp", cv2.IMREAD_GRAYSCALE)
+
+
+def plot_histogram(image, title):
+    histo = cv2.calcHist([image], [0], None, [256], [0, 256])
+    plt.plot(histo, color='black')
+    plt.title(title)
+    plt.xlabel("Niveaux de gris")
+    plt.ylabel("Nombre de pixels")
+    plt.show()
+    return histo
+
+
+"""
+histobat=plot_histogram(bateaunoir, "Histogramme de bateaunoir")
+histo_ping=plot_histogram(pingu, "Histogramme de pingu")
+histo_sea=plot_histogram(seanight, "Histogramme de seanight")"""
+
+
+
+def normalisation_et_histo(image):
+    min_val, max_val = np.min(image), np.max(image)
+
+    if max_val == min_val:
+        return np.histogram(image, bins=256, range=(0, 255))[0]
+
+    # recadrage dynamique formule
+    image_norm = ((image - min_val) * (255.0 / (max_val - min_val))).astype(np.uint8)
+
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
+    plt.imshow(image_norm, cmap='gray')
+    plt.title("Image Normalisée")
+    plt.axis("off")
+
+    histo = cv2.calcHist([image_norm], [0], None, [256], [0, 256])
+    plt.subplot(1,2,2)
+    plt.plot(histo, color='black')
+    plt.title("Histogramme avec recadrage dynamique")
+    plt.xlabel("Intensité")
+    plt.ylabel("Nombre de pixels")
+    plt.show()
+
+    return histo
+
+"""
+normalisation_et_histo(bateaunoir)
+normalisation_et_histo(pingu)
+normalisation_et_histo(seanight)"""
+
+def equalize_histogram_manual(image):
+    """
+    Effectue l'égalisation d'histogramme sur une image en niveaux de gris sans utiliser cv2.equalizeHist.
+    
+    Paramètre:
+    - image: Image en niveaux de gris (2D array de type uint8)
+    
+    Retourne:
+    - Image après égalisation d'histogramme.
+    """
+    
+    
+    histo = cv2.calcHist([image], [0], None, [256], [0, 256])
+    
+    cdf = histo.cumsum()  
+    hauteur, largeur= image.shape  
+    N= hauteur*largeur
+    
+    cdf_normalized = cdf  * 255 / N
+
+    cdf_normalized = cdf_normalized.astype(np.uint8)
+    
+    image_equalized = cdf_normalized[image]
+
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
+    plt.imshow(image_equalized, cmap='gray')
+    plt.title("Image Normalisée")
+    plt.axis("off")
+
+    histo = cv2.calcHist([image_equalized], [0], None, [256], [0, 256])
+    plt.subplot(1,2,2)
+    plt.plot(histo, color='black')
+    plt.title("Histogramme avec equalized")
+    plt.xlabel("Intensité")
+    plt.ylabel("Nombre de pixels")
+    plt.show()
+    
+    return image_equalized
+"""
+equalize_histogram_manual(pingu)
+equalize_histogram_manual(bateaunoir)
+equalize_histogram_manual(seanight)"""
+
+
+
+def transfert(t):
+    # Convertir t en une matrice avec 3 colonnes, une pour chaque composant RGB
+    s = t.shape
+    m = np.zeros((s[0], 3), dtype=float)
+    
+    m[:, 0] = t[:, 0]  # La première colonne pour le rouge
+    m[:, 1] = t[:, 0]  # La deuxième colonne pour le vert
+    m[:, 2] = t[:, 0]  # La troisième colonne pour le bleu
+    
+    # Normaliser les valeurs entre 0 et 1 pour la colormap
+    m = m / 255.0
+    
+    # Appliquer la colormap
+    plt.imshow(m, cmap='gray', aspect='auto')
+    plt.axis('off')
+    plt.show()
+
+# Affichez l'image
+plt.imshow(pingu, cmap='gray')
+plt.axis('off')
+plt.show()
+
+# Appliquez le transfert avec la matrice de valeurs [0:255]
+transfert(np.array([np.arange(0, 256)]).T)
+
